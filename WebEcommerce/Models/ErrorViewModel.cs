0776cf4bb836f2886eaa08391f6439aae06245f2
akaiqt882001234 <anhnpngcd191020@fpt.edu.vn>
2023-02-08 16:54:36 +0700

    Add project files.

---
+++ WebEcommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebEcommerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
