956da825e50a3e61d818140a288541b2f10d1354
akaiqt882001234 <anhnpngcd191020@fpt.edu.vn>
2023-02-10 11:50:02 +0700

    Done Categories & Products Controller

---
+++ WebEcommerce/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using WebEcommerce.Data;
+
+namespace WebEcommerce.Controllers
+{
+    public class CategoriesController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var Response =await _context.Categories.ToListAsync();
+            return View(Response);
+        }
+    }
+}
---
+++ WebEcommerce/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using WebEcommerce.Data;
+
+namespace WebEcommerce.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var Response =await _context.Products.ToListAsync();
+            return View(Response);
+        }
+    }
+}
