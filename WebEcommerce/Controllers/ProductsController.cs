ace418a281374a1dc8a63f8b7a7df6df510da0ee
akaiqt882001234 <anhnpngcd191020@fpt.edu.vn>
2023-02-12 19:28:55 +0700

    Done Create Product View

--- WebEcommerce/Controllers/ProductsController.cs
+++ WebEcommerce/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
 using WebEcommerce.Data;
+using WebEcommerce.Models;
 using WebEcommerce.Services;
 
 namespace WebEcommerce.Controllers
@@ -11,10 +12,12 @@ namespace WebEcommerce.Controllers
     {
 
         private readonly IProductServices _services;
+        private readonly ICategoryServices _categoryServices;
 
-        public ProductsController(IProductServices services)
+        public ProductsController(IProductServices services, ICategoryServices categoryServices)
         {
             _services = services;
+            _categoryServices = categoryServices;
         }
         public async Task<IActionResult> Index()
         {
@@ -24,8 +27,26 @@ namespace WebEcommerce.Controllers
 
         public async Task<IActionResult> Details(int id)
         {
-            var Product = await _services.GetByIdAsync(id,x=>x.Category);
+            var Product = await _services.GetByIdAsync(id, x => x.Category);
             return View(Product);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.CategoryId = await _categoryServices.GetAllAsync();
+            return View();
+        }
+
+        [HttpPost,ActionName(nameof(Create))]
+        public async Task<IActionResult> CreateProduct(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                await _services.CreateAsync(product);
+                return RedirectToAction(nameof(Index));
+            }
+            return View("NotFound");
         }
     }
 }
